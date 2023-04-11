26ad8c9cfe08b0edb4ed3ba022683dc24b27a610
plnklkv <polinakulakova04072@gmail.com>
2023-04-11 10:36:27 +0700

    manabar

--- resources/lifebar/lifebar.go
+++ resources/lifebar/lifebar.go
@@ -28,3 +28,23 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "manabar_progress"
+  component: "/resources/lifebar/manabar_progress.sprite"
+  position {
+    x: -1.0
+    y: 1.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 1.178
+    y: 1.0
+    z: 1.0
+  }
+}
