a596b955c0a952f697c761547ed5cb52f44d49f2
plnklkv <polinakulakova04072@gmail.com>
2023-05-16 17:12:32 +0700

    add 2 enemy, level 2 change: update menu, update_scills

---
+++ resources/enemy/enemy2.go
@@ -0,0 +1,134 @@
+components {
+  id: "enemy"
+  component: "/resources/enemy/enemy.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "lifebar"
+  component: "/resources/lifebar/lifebar.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "lifebar_background"
+  component: "/resources/lifebar/lifebar_background.sprite"
+  position {
+    x: 1.0
+    y: 35.0
+    z: 0.2
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "lifebar_progress"
+  component: "/resources/lifebar/lifebar_progress.sprite"
+  position {
+    x: 0.0
+    y: 36.0
+    z: 0.3
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/resources/enemy/enemy2.tilesource\"\n"
+  "default_animation: \"run\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "obstacle_collider"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"enemy\"\n"
+  "mask: \"obstacle_for_enemy\"\n"
+  "mask: \"platform\"\n"
+  "mask: \"player\"\n"
+  "mask: \"shot\"\n"
+  "mask: \"weapon\"\n"
+  "mask: \"enemy\"\n"
+  "mask: \"player2\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 4.0\n"
+  "      y: 3.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 21.645\n"
+  "  data: 24.678\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- resources/level2/level2.go
+++ resources/level2/level2.go
@@ -186,3 +186,30 @@ embedded_components {
     z: 1.0
   }
 }
+embedded_components {
+  id: "obstacle_for_enemy"
+  type: "collisionobject"
+  data: "collision_shape: \"/resources/level2/level2.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"obstacle_for_enemy\"\n"
+  "mask: \"enemy\"\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
